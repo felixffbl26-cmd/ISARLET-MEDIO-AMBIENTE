bf9095e81140bbb301ed29e010a9c8247f30b74c
felixffbl26-cmd <felixffbl.26@gmail.com>
2026-08-03 23:58:49 -0500

    Fix typo in author name

--- webapp/pages/1_🧪_Simulador.py
+++ webapp/pages/1_🧪_Simulador.py
@@ -3,7 +3,7 @@ AQUA-DAR PUNO v2.0 (Web Edition)
 ================================
 Pagina Simulador: inputs de configuracion + ejecucion + outputs interactivos.
 
-Autoras: Maria Isabel Nayde Zevallos Ttito / Arlet Mariciela Espezua Cuentas
+Autoras: Maria Isabel Nayde Zevallos Ttito / Arlet Maricielo Espezua Cuentas
 """
 
 import sys
